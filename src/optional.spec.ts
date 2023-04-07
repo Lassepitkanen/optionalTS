c0c7ef3fab599ba44f200d68360e3b6b87b4c78b
Lasse <lpitkanen@topcon.com>
2023-04-07 23:04:28 +0900

    create project

---
+++ src/optional.spec.ts
@@ -0,0 +1,163 @@
+import { None, Optional, Some } from './optional';
+
+describe('Optional', () => {
+    describe('of', () => {
+        it('should return a Some instance when given a non-undefined value', () => {
+            const optional = Optional.of('hello');
+            expect(optional instanceof Some).toBe(true);
+            expect(optional.get()).toBe('hello');
+        });
+
+        it('should return a None instance when given an undefined value', () => {
+            const optional = Optional.of(undefined);
+            expect(optional instanceof None).toBe(true);
+            expect(optional.isEmpty()).toBe(true);
+        });
+
+        it('should return Some if value is not undefined or null', () => {
+            const optional = Optional.of(5);
+            expect(optional.isPresent()).toBe(true);
+            expect(optional.get()).toBe(5);
+        });
+    });
+
+    describe('empty', () => {
+        it('should return a None instance', () => {
+            const optional = Optional.empty();
+            expect(optional instanceof None).toBe(true);
+            expect(optional.isEmpty()).toBe(true);
+        });
+    });
+
+    describe('map', () => {
+        it('should apply the mapper function to the content and return a Some instance with the result', () => {
+            const optional = Optional.of('hello').map((str) => str.toUpperCase());
+            expect(optional instanceof Some).toBe(true);
+            expect(optional.get()).toBe('HELLO');
+        });
+
+        it('should return a None instance when called on an empty Optional', () => {
+            const optional = Optional.of(undefined as unknown as string).map((str) => str.toUpperCase());
+            expect(optional instanceof None).toBe(true);
+            expect(optional.isEmpty()).toBe(true);
+        });
+    });
+
+    describe('isPresent', () => {
+        it('should return true when called on a Some instance', () => {
+            const optional = Optional.of('hello');
+            expect(optional.isPresent()).toBe(true);
+        });
+
+        it('should return false when called on a None instance', () => {
+            const optional = Optional.empty();
+            expect(optional.isPresent()).toBe(false);
+        });
+    });
+
+    describe('isEmpty', () => {
+        it('should return true when called on a None instance', () => {
+            const optional = Optional.empty();
+            expect(optional.isEmpty()).toBe(true);
+        });
+
+        it('should return false when called on a Some instance', () => {
+            const optional = Optional.of('hello');
+            expect(optional.isEmpty()).toBe(false);
+        });
+    });
+
+    describe('get', () => {
+        it('should return the content of a Some instance', () => {
+            const optional = Optional.of('hello');
+            expect(optional.get()).toBe('hello');
+        });
+
+        it('should throw an error when called on a None instance', () => {
+            const optional = Optional.empty();
+            expect(() => optional.get()).toThrowError('NoSuchElementException');
+        });
+    });
+
+    describe('ifPresent', () => {
+        it('should call the consumer function with the content of a Some instance', () => {
+            const mockConsumer = jest.fn();
+            const optional = Optional.of('hello');
+            optional.ifPresent(mockConsumer);
+            expect(mockConsumer).toHaveBeenCalledWith('hello');
+        });
+
+        it('should not call the consumer function when called on a None instance', () => {
+            const mockConsumer = jest.fn();
+            const optional = Optional.empty();
+            optional.ifPresent(mockConsumer);
+            expect(mockConsumer).not.toHaveBeenCalled();
+        });
+
+        it('should return the Optional instance to allow method chaining', () => {
+            const optional = Optional.of('hello');
+            const result = optional.ifPresent((str) => str);
+            expect(result).toBe(optional);
+        });
+    });
+
+    describe('orElse', () => {
+        it('should return the value of a Some', () => {
+            const optional = Optional.of('hello');
+            const value = optional.orElse('world');
+            expect(value).toBe('hello');
+        });
+
+        it('should return the alternative value for a None', () => {
+            const optional = Optional.empty();
+            const value = optional.orElse('world');
+            expect(value).toBe('world');
+        });
+    });
+
+    describe('orElseGet', () => {
+        it('should return the value of a Some', () => {
+            const optional = Optional.of('hello');
+            const value = optional.orElseGet(() => 'world');
+            expect(value).toBe('hello');
+        });
+
+        it('should return the result of the function for a None', () => {
+            const optional = Optional.empty();
+            const value = optional.orElseGet(() => 'world');
+            expect(value).toBe('world');
+        });
+    });
+
+    describe('orElseThrow', () => {
+        it('should return the value of a Some', () => {
+            const optional = Optional.of('hello');
+            const value = optional.orElseThrow(new Error('should not throw'));
+            expect(value).toBe('hello');
+        });
+
+        it('should throw the given error for a None', () => {
+            const optional = Optional.empty();
+            expect(() => optional.orElseThrow(new Error('should throw'))).toThrowError('should throw');
+        });
+    });
+
+    describe('equals', () => {
+        it('should return true for equal optionals', () => {
+            const optional1 = Optional.of('hello');
+            const optional2 = Optional.of('hello');
+            expect(optional1.equals(optional2)).toBe(true);
+        });
+
+        it('should return false for different optionals', () => {
+            const optional1 = Optional.of('hello');
+            const optional2 = Optional.of('world');
+            expect(optional1.equals(optional2)).toBe(false);
+        });
+
+        it('should return false for an optional and a non-optional', () => {
+            const optional = Optional.of('hello');
+            expect(optional.equals('hello' as unknown as Optional<string>)).toBe(false);
+        });
+    });
+});
---
+++ src/optional.ts
@@ -0,0 +1,178 @@
+export abstract class Optional<T> {
+    protected abstract content: T;
+
+    protected constructor() {
+        //
+    }
+
+    /**
+     * Returns an Optional with the specified value, or an empty Optional if the value is null or undefined.
+     *
+     * @param value - The value to wrap in an Optional.
+     * @returns {Optional<T>} - An Optional containing the specified value or an empty Optional.
+     */
+    static of<T>(value: T): Optional<T> {
+        return value != null ? new Some(value) : new None(value);
+    }
+
+    /**
+     * Returns an empty Optional.
+     *
+     * @returns {Optional<undefined>} - An empty Optional.
+     */
+    static empty(): Optional<undefined> {
+        return new None(undefined);
+    }
+
+    /**
+     * Applies the given mapping function to the content of this Optional and returns a new Optional that wraps the result.
+     *
+     * @param mapper - A function that maps the content of this Optional.
+     * @returns An Optional that wraps the result of the mapping function if this Optional is not empty, otherwise an empty Optional.
+     */
+    abstract map<K extends (param: NonNullable<T>) => any>(mapper: K): Optional<ReturnType<K>> | Optional<T>;
+
+    /**
+     * Returns true if this Optional contains a value, false otherwise.
+     *
+     * @returns true if this Optional contains a value, false otherwise.
+     */
+    abstract isPresent(): boolean;
+
+    /**
+     * Returns true if this Optional does not contain a value, false otherwise.
+     *
+     * @returns true if this Optional does not contain a value, false otherwise.
+     */
+    abstract isEmpty(): boolean;
+
+    /**
+     * Returns the content of this Optional, throwing an error if it is empty.
+     *
+     * @returns The content of this Optional.
+     * @throws An error if this Optional is empty.
+     */
+    abstract get(): NonNullable<T> | never;
+
+    /**
+     * Applies the given consumer function to the content of this Optional, if it is not empty.
+     *
+     * @param consumer - A function that consumes the content of this Optional.
+     * @returns This Optional, with methods to execute in a fluent style.
+     */
+
+    abstract ifPresent<K extends (param: NonNullable<T>) => any>(consumer: K): Optional<T>;
+
+    /**
+     * Returns the content of this Optional if it is not empty, or the specified value otherwise.
+     *
+     * @param value - The value to return if this Optional is empty.
+     * @returns The content of this Optional if it is not empty, or the specified value otherwise.
+     */
+    abstract orElse<K>(value: K): K | NonNullable<T>;
+
+    /**
+     * Returns the content of this Optional if it is not empty, or the result of the given function otherwise.
+     *
+     * @param func - A function that returns the value to use if this Optional is empty.
+     * @returns The content of this Optional if it is not empty, or the result of the given function otherwise.
+     */
+    abstract orElseGet<K extends () => any>(func: K): ReturnType<K> | NonNullable<T>;
+
+    /**
+     * Returns the content of this Optional if it is not empty, or throws the specified error otherwise.
+     *
+     * @param error - The error to throw if this Optional is empty.
+     * @returns The content of this Optional if it is not empty.
+     * @throws The specified error if this Optional is empty.
+     */
+    abstract orElseThrow<K extends Error>(error: K): never | NonNullable<T>;
+
+    /**
+     compares this optional with another to determine equality.
+     *
+     @param {Optional} optional - the optional to compare to.
+     @returns {boolean} - true if the two optional objects are equal, false otherwise.
+     */
+    equals(optional: Optional<T>): boolean {
+        return optional instanceof Optional && this.content === optional.content;
+    }
+}
+
+export class Some<T> extends Optional<T> {
+    constructor(protected content: NonNullable<T>) {
+        super();
+    }
+
+    map<K extends (param: NonNullable<T>) => any>(mapper: K): Some<ReturnType<K>> {
+        this.content = mapper(this.content);
+        return this as Some<ReturnType<K>>;
+    }
+
+    isEmpty(): boolean {
+        return false;
+    }
+
+    isPresent(): boolean {
+        return true;
+    }
+
+    get(): NonNullable<T> {
+        return this.content;
+    }
+
+    ifPresent<K extends (param: NonNullable<T>) => void>(consumer: K): Some<T> {
+        consumer(this.content);
+        return this;
+    }
+
+    orElse(): NonNullable<T> {
+        return this.content;
+    }
+
+    orElseGet(): NonNullable<T> {
+        return this.content;
+    }
+
+    orElseThrow(): NonNullable<T> {
+        return this.content;
+    }
+}
+
+export class None<T> extends Optional<T> {
+    constructor(protected readonly content: T) {
+        super();
+    }
+
+    map(): None<T> {
+        return this;
+    }
+
+    isEmpty(): boolean {
+        return true;
+    }
+
+    isPresent(): boolean {
+        return false;
+    }
+
+    get(): never {
+        throw new Error('NoSuchElementException');
+    }
+
+    ifPresent(): None<T> {
+        return this;
+    }
+
+    orElse<K>(value: K): K {
+        return value;
+    }
+
+    orElseGet<K extends () => any>(func: K): ReturnType<K> {
+        return func();
+    }
+
+    orElseThrow<K extends Error>(error: K): never {
+        throw error;
+    }
+}
